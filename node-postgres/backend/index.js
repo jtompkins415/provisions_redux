3cda4930558f77e2fe3eddd172afb201ddf500e0
jtompkins415 <john.tompkins415@gmail.com>
2023-11-10 00:38:42 -0800

    enable JSON parsing; add comments

--- node-postgres/backend/index.js
+++ node-postgres/backend/index.js
@@ -6,7 +6,11 @@ const userRoutes = require('../backend/routes/userRoutes');
 
 dotenv.config();
 
+//use environmental variables for port configuartion
 const port = process.env.SERVER_PORT;
+
+//enable JSON parsing
+app.use(express.json());
 
 //connection to user routes
 app.use('/users', userRoutes);
