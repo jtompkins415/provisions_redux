6ba1e4b1c5a4c65718e701917221be730d756aae
jtompkins415 <john.tompkins415@gmail.com>
2023-11-10 21:41:35 -0800

    Create CRUD methods & routes for foods table

--- node-postgres/backend/models/foodModel.js
+++ node-postgres/backend/models/foodModel.js
@@ -10,7 +10,7 @@ class FoodItem {
         this.category = category;
         this.price = price;
         this.description = description;
-    }
+    };
 
     //GET ALL FOODS
     static async getAllFoods(){
@@ -21,7 +21,50 @@ class FoodItem {
         }
 
         return allFoods.rows.map(food => new FoodItem(food.id, food.name, food.category, food.price, food.description));
-    }
+    };
+
+    //GET FOOD ITEM BY ID
+    static async getFoodItemById(id){
+        const food = await db.query(`SELECT * FROM foods WHERE id=$1`, [id])
+
+        if(food.rows.length == 0){
+            throw new Error('INVALID FOOD ITEM');
+        }
+
+        const foodData = food.rows[0];
+
+        return new FoodItem(foodData.id, foodData.name, foodData.category, foodData.price, foodData.description);
+    };
+
+    //CREATE FOOD ITEM
+    static async createFoodItem(name, category, price, description){
+        const newFoodItem = await db.query(`INSERT INTO foods (name, category, price, description) VALUES ($1, $2, $3, $4) RETURNING id`, [name, category, price, description]);
+
+        if(!newFoodItem.rows || newFoodItem.rows.length === 0){
+            throw new Error('ERROR IN FOOD ITEM CREATION');
+        } 
+        
+        let {id} = newFoodItem.rows[0];
+
+        return new FoodItem(id, name, category, price, description);
+    };
+
+    //UPDATE FOOD ITEM
+    async updateFoodItem(field, value){
+        const validFields = ['name', 'category', 'price', 'description'];
+
+        if(!validFields.includes(field)){
+            throw new Error('INVALID FIELD TO UPDATE');
+        }
+
+        const updateQuery = `UPDATE foods SET ${field}=$1 WHERE id=$2`;
+        await db.query(updateQuery, [value, this.id]);
+    };
+
+    //DELETE FOOD ITEM
+    async deleteFoodItem(){
+        await db.query(`DELETE FROM foods WHERE id=$1`, [this.id]);
+    };
 };
 
 module.exports = FoodItem;
--- node-postgres/backend/routes/foodsRoutes.js
+++ node-postgres/backend/routes/foodsRoutes.js
@@ -16,4 +16,57 @@ router.get('/', async function(req, res, next){
     }
 });
 
+//GET FOOD ITEM BY ID
+router.get('/:id', async function(req, res, next){
+    try {
+        const id = req.params.id;
+        const result = await FoodItem.getFoodItemById(id);
+        return res.status(200).json(result);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+//CREATE FOOD ITEM
+router.post('/create', upload.none(), async function(req, res, next){
+    try {
+        const {name, category, price, description} = req.body;
+        const newFoodItem = await FoodItem.createFoodItem(name, category, price, description);
+        return res.status(201).json(newFoodItem);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+//UPDATE FOOD ITEM
+router.patch('/update/:id/:field', upload.none(), async function(req, res, next){
+    try {
+        const id = req.params.id;
+        const field = req.params.field;
+        const {[field]: newValue} = req.body;
+
+        const foodItemToUpdate = await FoodItem.getFoodItemById(id);
+        await foodItemToUpdate.updateFoodItem(field, newValue);
+
+        return res.status(200).json({
+            message: `${field} updated successfully`, foodItemToUpdate
+        });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+//DELETE FOOD ITEM
+router.delete('/remove/:id', async function(req, res, next){
+    try {
+        const id = req.params.id;
+        const foodItemToDelete = await FoodItem.getFoodItemById(id);
+
+        await foodItemToDelete.deleteFoodItem();
+        return res.status(200).json({message: `${foodItemToDelete.name} removed successfully`, foodItemToDelete});
+    } catch (err) {
+        return next(err);
+    }
+})
+
 module.exports = router;
