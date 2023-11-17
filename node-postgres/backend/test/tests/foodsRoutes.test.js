7e8db1c15543b6e04f1ae40dd974fcc338e10394
John Tompkins <john.tompkins415@gmail.com>
2023-11-17 12:45:32 -0800

    Write PATCH & DELETE tests

--- node-postgres/backend/test/tests/foodsRoutes.test.js
+++ node-postgres/backend/test/tests/foodsRoutes.test.js
@@ -34,7 +34,42 @@ describe('Food Routes', () => {
         expect(response.body).toEqual(createdFoodItem);
     });
 
+    test('GET /foods should return an array with all food items', async () => {
+
+        const response = await request(app).get('/foods');
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0);
+
+        response.body.forEach((food) => {
+            expect(food).toHaveProperty('name');
+            expect(food).toHaveProperty('category');
+            expect(food).toHaveProperty('price');
+            expect(food).toHaveProperty('description');
+        });
+    });
+
+    test('PATCH /foods/update/:id/:field should return confirmation of the field being successfully updated', async () => {
+        expect(global.testFood).toBeDefined();
+
+        const response = await request(app).patch(`/foods/update/${global.testFood.id}/price`).send({
+            price: 3.49
+        });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({message: 'price updated successfully'});
+    });
+
+    test('DELETE /foods/remove/:id should return confirmation of food item removal', async () => {
+        expect(global.testFood).toBeDefined();
+
+        const response = await request(app).delete(`/foods/remove/${global.testFood.id}`);
+        
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({message: `${global.testFood.name} removed successfully`});
+    });
+
     afterAll(async () => {
         await db.end();
-    })
+    });
 });
