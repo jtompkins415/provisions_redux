e1aea4a3cd149291b7e49ea688c766c1e26b3770
John Tompkins <john.tompkins415@gmail.com>
2023-11-19 21:52:42 -0800

    create landing page component

---
+++ react-frontend/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+//Landing Page Component
+import {Button} from '@mui/material';
+
+function Home(){
+    
+    return (
+        <>
+            <div className="home-title-container">
+                <h1 id="home-title">Provisons</h1>
+                <h2 id="home-subtitle">Artisinal Foods, Craft Beers, & Natural Wines</h2>
+            </div>
+
+            <div className="home-shop-button-container">
+                <Button variant="contained" href='/shop'>Shop Now</Button>
+            </div>
+        </>
+    );
+}
+
+export default Home;
