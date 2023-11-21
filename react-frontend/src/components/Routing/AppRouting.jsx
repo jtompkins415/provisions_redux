1d3e0cc98fcf69431ab7c483d7c9a1f3977ce712
John Tompkins <john.tompkins415@gmail.com>
2023-11-20 18:39:12 -0800

    add route for testing helper function

--- react-frontend/src/components/Routing/AppRouting.jsx
+++ react-frontend/src/components/Routing/AppRouting.jsx
@@ -2,11 +2,13 @@
 
 import {Routes, Route} from 'react-router-dom';
 import Home from '../Home/Home';
+import Users from '../Users/User';
 
 function AppRouting(){
     return (
         <Routes>
             <Route exact path='/' element={<Home />} />
+            <Route exact path='/users' element={<Users/>} />
         </Routes>
     );
 }
