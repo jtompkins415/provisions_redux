ab306ce073c3d88c7a2c2d92e79a8505e0c2e955
John Tompkins <john.tompkins415@gmail.com>
2023-11-20 21:36:26 -0800

    refactor getUserData for simpler use

--- react-frontend/src/helpers/GetData.jsx
+++ react-frontend/src/helpers/GetData.jsx
@@ -1,35 +1,8 @@
-import { useState, useEffect } from "react";
 import axios from 'axios';
 
-export function GetUserData(){
-    const [users, setUsers] = useState([]);
+export async function GetUserData(){
+    const response = await axios.get('http://localhost:3001/users');
+    const userData = response.data;
 
-    useEffect(() => {
-        async function fetchUsers () {
-        try {
-            const usersData = await axios.get('/users');
-            setUsers(usersData.data)
-            console.log(users);
-        } catch (err) {
-            console.error('Error fetching users', err);
-        }
-        
-        }
-        fetchUsers();
-}, []);
-
-    return (
-        <>
-        {users.forEach((user) => (
-                <>
-                <div>{user.username}</div>
-                <div>{user.email}</div>
-                <div>{user.first_name}</div>
-                <div>{user.last_name}</div>
-                </>   
-            ))}
-        </>
-            
-        
-    )
+    return userData;
 }
