bc4ca505c021af6cf5ff9c999c2c56422d06c686
John Tompkins <john.tompkins415@gmail.com>
2023-12-01 10:28:37 -0800

    implement registration, authentication, and logout functionality

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,15 +1,95 @@
+import {useState, useEffect} from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 import {BrowserRouter} from 'react-router-dom';
+import {decodeToken} from 'react-jwt';
+import ProvisionsReduxApi from './api/provisions-redux-api';
 import NavBar from './components/NavBar/NavBar'
 import AppRouting from './components/Routing/AppRouting';
 import './App.css'
 
+export const TOKEN_STORAGE = 'token';
+
 function App() {
+  
+  const [currentUser, setCurrentUser] = useState(null);
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE);
+  const [infoLoaded, setInfoLoaded] = useState(false);
+
+   /**
+   * Effect to get current user.
+   * 
+   * If there is a token present in local storage,
+   * get username from token,
+   * use Provisions-Redux-API to talk to backend
+   * retrieve user obj:
+   * 
+   * {username, }
+   * 
+   */
+
+   useEffect(() => {
+    
+    const getCurrUser = async () => {
+      if (token){
+        try{
+          let {username} = decodeToken(token);
+          let currUser = await ProvisionsReduxApi.getCurrUser(username);
+          console.log(currUser);
+          setCurrentUser(currUser);
+        }catch(err){
+          console.error("App getUserInfo: problem loading", err);
+          setCurrentUser(null);
+        }
+      }
+      setInfoLoaded(true);
+    }
+    setInfoLoaded(false);
+    getCurrUser()
+  }, [token]);
+
+
+  /** Handles site-wide logout  */
+
+   const logout = () => {
+    setCurrentUser(null);
+    setToken(null);
+    console.log(currentUser, token);
+   }
+
+  /** Handle site-wide login */
+
+  const login = async (username, password) => {
+    try{
+      let res = await ProvisionsReduxApi.userLogin(username, password);
+      setToken(res);
+      return {success: true};
+    }catch(err){
+      console.error('Login Failed', err);
+      return {success: false, err}
+    }
+  }
+
+  /** Handle site-wide sign up 
+   * Sets Token upon sign up
+  */
+
+  const signup = async (username, email, password, first_name, last_name, city, state) => {
+    try{
+      let res = await ProvisionsReduxApi.userSignup(username, email, password, first_name, last_name, city, state)
+      setToken(res);
+      return {success: true};
+    }catch(err){
+      console.error('Signup Failed', err);
+      return {success: false, err};
+    }
+  }
+
 
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        <AppRouting />
+        <NavBar currentUser={currentUser} logout={logout}/>
+        <AppRouting login={login} signup={signup} currentUser={currentUser} />
       </BrowserRouter>
       
     </>
