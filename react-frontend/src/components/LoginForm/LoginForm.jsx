dfd02a191fff0f06c3b9563096ee37ea254ddd4d
John Tompkins <john.tompkins415@gmail.com>
2023-12-01 10:31:09 -0800

    add form submission functionality & styling

--- react-frontend/src/components/LoginForm/LoginForm.jsx
+++ react-frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,37 @@
-import {useState, useEffect} from 'react';
-import {Box, TextField, Button } from '@mui/material';
+import { redirect } from 'react-router-dom';
+import {useState} from 'react';
+import {Box, TextField } from '@mui/material';
+import './LoginForm.css';
 
 
-function LoginForm (){
+
+function LoginForm ({login}){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (evnt) => {
+        evnt.preventDefault();
+        const loginResults = await login(username, password);
+
+        console.log(loginResults.success);
+        redirect('/home');
+    }
+    
+
     return (
         <div id='login-form-container'>
-            <Box component='form' id='login-form'>
-                <div id="form-input-containers">
-                    <TextField required className='form-input' label='username' defaultValue='Username' />
-                    <TextField required className='form-input' label='password' defaultValue='Password' />
-                    <button type='submit'>LOGIN</button>
+            <div id="background-color">
+                <div id='form-title-container'>
+                    <h2 id='form-title'>Login</h2>
                 </div>
-            </Box>
+                <Box component='form' id='login-form' onSubmit={handleSubmit}>
+                    <div id="form-input-containers">
+                        <TextField required className='form-input' label='username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <TextField required className='form-input' label='password' vaule={password} onChange={(e) => setPassword(e.target.value)} />
+                        <button type='submit' id='login-submit'>Submit</button>
+                    </div>
+                </Box>
+            </div>
         </div>
     )
 }
