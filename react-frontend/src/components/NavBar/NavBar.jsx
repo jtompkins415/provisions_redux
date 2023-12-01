4bed835ce833b99e4cae82ebcebcefa7e3c64435
John Tompkins <john.tompkins415@gmail.com>
2023-12-01 10:31:41 -0800

    add conditional rendering based on logged in user

--- react-frontend/src/components/NavBar/NavBar.jsx
+++ react-frontend/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,31 @@
 import {Link} from '@mui/material/';
 import './NavBar.css';
 
-function NavBar(){
+function NavBar({currentUser, logout}){
+
+
     return (
        <div id='nav-main-container'>
             <div id='icon-container'>
                 <span id='nav-icon'>Provisions</span>
             </div>
             <div id='nav-links-container'>
-                <Link href='/' className='nav-link'>Home</Link>
+                <Link href='/home' className='nav-link'>Home</Link>
                 <Link href='/about' className='nav-link'>About</Link>
                 <Link href='/shop' className='nav-link'>Shop</Link>
-                <Link href='/login' className='nav-link'>Login</Link>
-                <Link href='/login' className='nav-link'>Sign-up</Link>
+
+                {currentUser ? (
+                    <>
+                        <Link href={`/users/${currentUser.username}`} className='nav-link'>Account</Link>
+                        <Link href='/' className='nav-link' onClick={logout}>Logout</Link>
+                    </>
+                    ):(
+                    <>
+                        <Link href='/login' className='nav-link'>Login</Link>
+                        <Link href='/signup' className='nav-link'>Sign-up</Link>
+                    </>
+                )}
+                
             </div>
        </div>
             
