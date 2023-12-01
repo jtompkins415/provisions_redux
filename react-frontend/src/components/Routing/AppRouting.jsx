6864555949b0ba9d296e9fc5e3e4775903661950
John Tompkins <john.tompkins415@gmail.com>
2023-12-01 10:32:02 -0800

    add routes for login and signup

--- react-frontend/src/components/Routing/AppRouting.jsx
+++ react-frontend/src/components/Routing/AppRouting.jsx
@@ -3,12 +3,14 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from '../Home/Home';
 import LoginForm from '../LoginForm/LoginForm';
+import SignupForm from '../SignupForm/SignupForm';
 
-function AppRouting(){
+function AppRouting({login, signup}){
     return (
         <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/login' element={<LoginForm />} />
+            <Route exact path='/home' element={<Home />} />
+            <Route exact path='/login' element={<LoginForm login={login} />} />
+            <Route exact path='/signup' element={<SignupForm signup={signup}/>} />
         </Routes>
     );
 }
