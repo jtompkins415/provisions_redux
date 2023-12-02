8b7ffbba3ddb7293d1f5b48ee18d96c7b6f15a5e
John Tompkins <john.tompkins415@gmail.com>
2023-12-02 00:31:06 -0800

    add conditional routing to account details

--- react-frontend/src/components/Routing/AppRouting.jsx
+++ react-frontend/src/components/Routing/AppRouting.jsx
@@ -4,13 +4,17 @@ import {Routes, Route} from 'react-router-dom';
 import Home from '../Home/Home';
 import LoginForm from '../LoginForm/LoginForm';
 import SignupForm from '../SignupForm/SignupForm';
+import AccountPage from '../AccountPage/AccountPage';
 
-function AppRouting({login, signup}){
+function AppRouting({login, signup, currentUser}){
     return (
         <Routes>
-            <Route exact path='/home' element={<Home />} />
+            <Route exact path='/' element={<Home />} />
             <Route exact path='/login' element={<LoginForm login={login} />} />
             <Route exact path='/signup' element={<SignupForm signup={signup}/>} />
+            {currentUser && (
+                <Route exact path={`/account/${currentUser.username}`} element={<AccountPage currentUser={currentUser} />} />
+            )}
         </Routes>
     );
 }
