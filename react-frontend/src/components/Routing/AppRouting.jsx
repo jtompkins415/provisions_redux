7a6f4c25e9eedadda5dd8742bf6ebe4dab082758
John Tompkins <john.tompkins415@gmail.com>
2023-12-08 10:58:56 -0800

    Add routing for about page

--- react-frontend/src/components/Routing/AppRouting.jsx
+++ react-frontend/src/components/Routing/AppRouting.jsx
@@ -5,6 +5,7 @@ import Home from '../Home/Home';
 import LoginForm from '../LoginForm/LoginForm';
 import SignupForm from '../SignupForm/SignupForm';
 import AccountPage from '../AccountPage/AccountPage';
+import AboutPage from '../AboutPage/AboutPage';
 
 function AppRouting({login, signup, currentUser}){
     return (
@@ -15,6 +16,7 @@ function AppRouting({login, signup, currentUser}){
             {currentUser && (
                 <Route exact path={`/account/${currentUser.username}`} element={<AccountPage currentUser={currentUser} />} />
             )}
+            <Route exact path='/about' element={<AboutPage />}/>
         </Routes>
     );
 }
