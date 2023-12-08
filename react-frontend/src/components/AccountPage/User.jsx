88be4aafb2d0fa53b49b0ea71a5c7faeff35c8de
John Tompkins <john.tompkins415@gmail.com>
2023-12-08 10:58:41 -0800

    Add more structure and bold tags to component and styles

--- react-frontend/src/components/AccountPage/User.jsx
+++ react-frontend/src/components/AccountPage/User.jsx
@@ -3,15 +3,15 @@ import './User.css';
 function User({currentUser}) {
     return (
         <div id="user-main-container">
-            <div id="user-title-contianer">
+            <div id="user-title-container">
                 <span id="user-username">{currentUser.username}</span>
                 <span id="user-email">{currentUser.email}</span>
             </div>
             <div id="user-detail-container">
-                <span>First Name: {currentUser.first_name}</span>
-                <span>Last Name: {currentUser.last_name}</span>
-                <span>City: {currentUser.city}</span>
-                <span>State: {currentUser.state}</span>
+                <span><b>First Name:</b> {currentUser.first_name}</span>
+                <span><b>Last Name:</b> {currentUser.last_name}</span>
+                <span><b>City:</b> {currentUser.city}</span>
+                <span><b>State:</b> {currentUser.state}</span>
             </div>
         </div>
     )
