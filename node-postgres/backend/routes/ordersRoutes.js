4fd472c809dba435881894caa167b392c85e05b4
John Tompkins <john.tompkins415@gmail.com>
2023-12-12 20:36:42 -0800

    create GET routes; create POST route; create DELETE route;

--- node-postgres/backend/routes/ordersRoutes.js
+++ node-postgres/backend/routes/ordersRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Order = require('../models/orderModel');
+const OrderItem = require('../models/orderItemModel');
 
 const multer = require('multer');
 const upload = multer();
@@ -15,5 +16,67 @@ router.get('/', async function(req, res, next){
         return next(err);
     }
 });
+
+//GET ORDER BY ORDER ID
+router.get('/:order_id', async function(req, res, next){
+    try {
+        const {order_id} = req.params;
+        const result = await Order.getOrderById(order_id);
+        return res.status(200).json(result);
+    } catch (err) {
+        return next(err);
+    };
+});
+
+//GET ORDER BY USER ID
+router.get('/:user_id', async function(req, res, next){
+    try {
+        const {user_id} = req.params;
+        const result = await Order.getOrdersByUser(user_id);
+        return res.status(200).json(result);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+//GET ITEMS FROM ORDER BY ORDER ID
+router.get('/items/:order_id', async function(req, res, next){
+    try {
+        const {order_id} = req.params;
+        const results = await OrderItem.getItemsByOrderId(order_id);
+        return res.status(200).json(results);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+//CREATE NEW ORDER
+router.post('/create', upload.none(), async function(req, res, next){
+    try{
+        const {user_id, total_price, order_date} = req.body;
+        const newOrder = await Order.createOrder(user_id, total_price, order_date);
+
+        if(!newOrder){
+            return res.status(409).json({error: 'Error in Order Creation'})
+        } else {
+            return res.status(201).json(newOrder);
+        }   
+    } catch (err) {
+        console.log('Order Creation Error', err);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+//DELETE ORDER
+router.delete('/remove/:order_id', async function(req, res, next){
+    try {
+        const {order_id} = req.params;
+        const orderToDelete = await Order.getOrderById(order_id);
+        await orderToDelete.deleteOrder();
+        return res.status(200).json({message: `Order ${order_id} deleted successfully`});
+    } catch (err) {
+        return next(err);
+    }
+})
 
 module.exports = router;
