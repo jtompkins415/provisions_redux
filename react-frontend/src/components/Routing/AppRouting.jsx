30c1d0d649037e0846f32711bcf7943ee4e7f5f0
John Tompkins <john.tompkins415@gmail.com>
2023-12-18 11:25:37 -0800

    Pass in current cart model to Shop route and cart route

--- react-frontend/src/components/Routing/AppRouting.jsx
+++ react-frontend/src/components/Routing/AppRouting.jsx
@@ -7,8 +7,9 @@ import SignupForm from '../SignupForm/SignupForm';
 import AccountPage from '../AccountPage/AccountPage';
 import AboutPage from '../AboutPage/AboutPage';
 import ShopPage from '../Shop/ShopPage';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
 
-function AppRouting({login, signup, currentUser}){
+function AppRouting({login, signup, currentUser, cart}){
     return (
         <Routes>
             <Route exact path='/' element={<Home />} />
@@ -18,7 +19,8 @@ function AppRouting({login, signup, currentUser}){
                 <Route exact path={`/account/${currentUser.username}`} element={<AccountPage currentUser={currentUser} />} />
             )}
             <Route exact path='/about' element={<AboutPage />}/>
-            <Route exact path='/shop' element={<ShopPage />} />
+            <Route exact path='/shop' element={<ShopPage cart={cart}/>} />
+            <Route exact path='/shop/cart' element={<ShoppingCart cart={cart} />} /> 
         </Routes>
     );
 }
