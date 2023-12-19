a74328ea26867d240a9bc4fc4fdc8cf4f2b2402f
John Tompkins <john.tompkins415@gmail.com>
2023-12-18 16:41:02 -0800

    destructure cart object for cart methods; render cart items; add styles

--- react-frontend/src/components/ShoppingCart/ShoppingCart.jsx
+++ react-frontend/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,27 +1,32 @@
 import { Button } from "@mui/material";
+import './ShoppingCart.css';
 
 
 function ShoppingCart({cart}){
-    const {currentCart,clearCart} = cart
+    const {currentCart,clearCart, removeItemFromCart, getCartTotal} = cart
     const cartItems = currentCart;
     
     console.log(currentCart);
     return (
         <div id="shoppingCart-main-container">
-            <div id="shoppingCart-title-container">
-                <h2>SHOPPING CART</h2>
-            </div>
             <div id="shoppingCart-cart-container">
                 <div id="shoppingCart-cartTitle-container">
                     <h3>Your Order</h3>
                 </div>
                 <div id="shoppingCart-cartDetails-container">
-                    {/* {cartItems.map((cartItem) => (
+                    {cartItems.map((cartItem) => (
                         <>
-                            <p>{cartItem}</p>
+                            <p key={cartItem.id} className="cartItem-details">{cartItem.name} - ${cartItem.price}</p>
+                            <Button onClick={() => removeItemFromCart(cartItem.id)} id="cartItem-removeButton">X</Button>
                         </>
-                    ))} */}
-                    <Button onClick={() => clearCart()}>Clear Cart</Button>
+                    ))}
+
+                </div>  
+                <div id="shoppingCart-total-container">
+                    <p id="shoppingCart-total">Total: ${getCartTotal()} </p>
+                </div>  
+                <div id="shoppingCart-clearButton-container">
+                    <Button onClick={() => clearCart()} id="shopping">Empty Cart</Button>
                 </div>
             </div>
             
